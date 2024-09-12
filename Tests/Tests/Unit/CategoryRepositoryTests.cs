36248a33996ce309e822715d3a6059853b341cab
AlecDr <alecsander_rs@hotmail.com>
2024-09-12 18:56:28 -0300

    add missing categories repository test

--- Tests/Tests/Unit/CategoryRepositoryTests.cs
+++ Tests/Tests/Unit/CategoryRepositoryTests.cs
@@ -26,5 +26,31 @@ public class CategoryRepositoryTests
         Assert.Empty(categories);
     }
 
+    [Fact]
+    public async Task AllCategories_MustReturnNotEmptyList()
+    {
+        string response = JsonSerializer.Serialize(new
+        {
+            Drinks = new[] {
+                new
+                {
+                    Name = "Category 1"
+                },
+                new
+                {
+                    Name = "Category 2"
+                },
+            }
+        });
+        StaticDrinksJsonHttpMessageHandler httpMessageHandler = new StaticDrinksJsonHttpMessageHandler(response);
+        HttpClient client = new HttpClient(httpMessageHandler);
+        CategoryRepository categoryRepository = new CategoryRepository(client);
+
+        // Act
+        List<CategoryDTO> categories = await categoryRepository.All();
+
+        // Assert
+        Assert.NotEmpty(categories);
+    }
 
 }
